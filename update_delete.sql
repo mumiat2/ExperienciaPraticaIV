b011965aa9000ad35b42c23d9d92663043f3b91b
mumiat2 <125617620+mumiat2@users.noreply.github.com>
2025-11-30 20:40:29 -0300

    Update SQL queries for product and client management

--- update_delete.sql
+++ update_delete.sql
@@ -2,29 +2,29 @@
 
 -- 1. Atualizar estoque após venda
 UPDATE Produto 
-SET estoque = estoque - 2 
+SET quantidade_estoque = quantidade_estoque - 1 
 WHERE id_produto = 1;
 
--- 2. Atualizar telefone do cliente
-UPDATE Cliente 
-SET telefone = '116666666' 
-WHERE id_cliente = 2;
+-- 2. Atualizar telefone do tutor
+UPDATE Tutor 
+SET telefone = '117777777' 
+WHERE id_tutor = 2;
 
--- 3. Alterar data de um pedido
-UPDATE Pedido 
-SET data_pedido = '2025-12-01' 
-WHERE id_pedido = 2;
+-- 3. Alterar diagnóstico de uma consulta
+UPDATE Consulta 
+SET diagnostico = 'Infecção tratada' 
+WHERE id_consulta = 2;
 
 -- Exclusões
 
--- 1. Remover item de pedido
-DELETE FROM ItemPedido 
-WHERE id_pedido = 1 AND id_produto = 2;
+-- 1. Remover aplicação de vacina
+DELETE FROM AplicacaoVacina 
+WHERE id_aplicacao = 2;
 
--- 2. Remover pedido
-DELETE FROM Pedido 
-WHERE id_pedido = 2;
+-- 2. Remover item de venda
+DELETE FROM ItemVenda 
+WHERE id_item = 2;
 
--- 3. Remover cliente
-DELETE FROM Cliente 
-WHERE id_cliente = 3;
+-- 3. Remover agendamento cancelado
+DELETE FROM Agendamento 
+WHERE id_agendamento = 2;
